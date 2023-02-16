ac09e023492bfcf59197afaa304132d989f29e10
Mario <mario1.lanpert@gmail.com>
2023-02-16 03:50:30 +0100

    added firefox and other api

--- extension_menu/menu.js
+++ extension_menu/menu.js
@@ -4,20 +4,44 @@ let isenabled;
 function changeStatus() {
     const checkboxText = document.getElementById('checkboxText');
     if (checkbox.checked) {
-        chrome.storage.local.set({ isenabled: true }, () => {
-            if (chrome.runtime.lastError)
-                console.log('Error QIS Helper - isenable storage menu:1');
-        });
+        if (typeof chrome !== 'undefined' && chrome.storage) {
+            // If Chrome-API available
+            chrome.storage.local.set({ isenabled: true }, () => {
+                if (chrome.runtime.lastError)
+                    console.log('Error QIS Helper - isenable storage menu:1.1');
+            });
+        } else if (typeof browser !== 'undefined' && browser.storage) {
+            // If Firefox-API available
+            browser.storage.local.set({ isenabled: true }).then(() => {
+                if (browser.runtime.lastError)
+                    console.log('Error QIS Helper - isenable storage menu:1.2');
+            });
+        } else {
+            // If no API is available
+            localStorage.setItem('isenabled', true);
+        }
 
         if (checkboxText.innerText.includes('disabled')) {
             checkboxText.innerText = checkboxText.innerText.replace('disabled', 'enabled');
         }
         checkbox.checked = true;
     } else {
-        chrome.storage.local.set({ isenabled: false }, () => {
-            if (chrome.runtime.lastError)
-                console.log('Error QIS Helper - isenable storage menu:2');
-        });
+        if (typeof chrome !== 'undefined' && chrome.storage) {
+            // If Chrome-API available
+            chrome.storage.local.set({ isenabled: false }, () => {
+                if (chrome.runtime.lastError)
+                    console.log('Error QIS Helper - isenable storage menu:2');
+            });
+        } else if (typeof browser !== 'undefined' && browser.storage) {
+            // If Firefox-API available
+            browser.storage.local.set({ isenabled: false }).then(() => {
+                if (browser.runtime.lastError)
+                    console.log('Error QIS Helper - isenable storage menu:1');
+            });
+        } else {
+            // If no API is available
+            localStorage.setItem('isenabled', false);
+        }
 
         if (checkboxText.innerText.includes('enabled')) {
             checkboxText.innerText = checkboxText.innerText.replace('enabled', 'disabled');
@@ -33,14 +57,35 @@ window.addEventListener('load', (event) => {
 });
 
 function getStorageValue() {
-    chrome.storage.local.get(["isenabled"]).then((result) => {
-        if (result.isenabled === undefined) {
+    if (typeof chrome !== 'undefined' && chrome.storage) {
+        // If Chrome-API available
+        chrome.storage.local.get(["isenabled"]).then((result) => {
+            if (result.isenabled === undefined) {
+                window.isenabled = true;
+            } else {
+                window.isenabled = result.isenabled;
+            }
+            setStatus();
+        });
+    } else if (typeof browser !== 'undefined' && browser.storage) {
+        // If Firefox-API available
+        browser.storage.local.get(["isenabled"]).then((result) => {
+            if (result.isenabled === undefined) {
+                window.isenabled = true;
+            } else {
+                window.isenabled = result.isenabled;
+            }
+            setStatus();
+        });
+    } else {
+        // If no API is available
+        if (localStorage.getItem('isenabled') === null) {
             window.isenabled = true;
         } else {
-            window.isenabled = result.isenabled;
+            window.isenabled = JSON.parse(localStorage.getItem('isenabled'));
         }
         setStatus();
-    });
+    }
 }
 
 function setStatus() {
